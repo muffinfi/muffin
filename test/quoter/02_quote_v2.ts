fb9f455a1066701a0b5d0e246e94b9ef3e6bc93c
dkenw <dkenw1618@gmail.com>
2022-02-03 19:27:39 +0800

    add swap quoter v2

--- test/quoter/02_quote_v2.ts
+++ test/quoter/02_quote_v2.ts
@@ -1,20 +1,19 @@
 import { expect } from 'chai';
 import { solidityPack } from 'ethers/lib/utils';
-import { ethers, waffle } from 'hardhat';
-import { IMockMuffinHub, IQuoter, MockERC20 } from '../../typechain';
+import { waffle } from 'hardhat';
+import { IMockMuffinHub, MockERC20, QuoterV2 } from '../../typechain';
 import { managerFixture } from '../shared/fixtures';
 import { deploy } from '../shared/utils';
 
-describe('swap quoter v1', () => {
+describe('swap quoter v2', () => {
   let token0: MockERC20;
   let token1: MockERC20;
-  let quoter: IQuoter;
+  let quoter: QuoterV2;
 
   beforeEach(async () => {
     let hub: IMockMuffinHub;
     ({ hub, token0, token1 } = await waffle.loadFixture(managerFixture));
-    const _quoter = await deploy('Quoter', hub.address);
-    quoter = (await ethers.getContractAt('IQuoter', _quoter.address)) as IQuoter;
+    quoter = (await deploy('QuoterV2', hub.address)) as QuoterV2;
   });
 
   context('quoteSingle', async () => {
@@ -22,14 +21,14 @@ describe('swap quoter v1', () => {
       const result = await quoter.quoteSingle(token0.address, token1.address, 0x3f, 3);
       expect(result.amountIn).eq(3);
       expect(result.amountOut).eq(1);
-      expect(result.gasUsed).gt(0);
+      expect(result.tierAmountsIn[0]).eq(3);
     });
 
     it('exact out', async () => {
       const result = await quoter.quoteSingle(token0.address, token1.address, 0x3f, -1);
       expect(result.amountIn).eq(3);
       expect(result.amountOut).eq(1);
-      expect(result.gasUsed).gt(0);
+      expect(result.tierAmountsIn[0]).eq(3);
     });
   });
 
@@ -53,7 +52,7 @@ describe('swap quoter v1', () => {
       const result = await quoter.quote(path, 3);
       expect(result.amountIn).eq(3);
       expect(result.amountOut).eq(1);
-      expect(result.gasUsed).gt(0);
+      expect(result.hops[0].tierAmountsIn[0]).eq(3);
     });
 
     it('exact out', async () => {
@@ -61,7 +60,7 @@ describe('swap quoter v1', () => {
       const result = await quoter.quote(path, 3);
       expect(result.amountIn).eq(3);
       expect(result.amountOut).eq(1);
-      expect(result.gasUsed).gt(0);
+      expect(result.hops[0].tierAmountsIn[0]).eq(3);
     });
   });
 });
