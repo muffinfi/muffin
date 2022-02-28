ff6fd243aed5d6bd6c66e7c03682821851843b89
dkenw <dkenw1618@gmail.com>
2022-02-28 12:23:51 +0800

    fix cvf-126: use built-in overflow check in mulDivRoundingUp

--- contracts/libraries/math/FullMath.sol
+++ contracts/libraries/math/FullMath.sol
@@ -129,12 +129,9 @@ library FullMath {
         uint256 b,
         uint256 denominator
     ) internal pure returns (uint256 result) {
-        unchecked {
-            result = mulDiv(a, b, denominator);
-            if (mulmod(a, b, denominator) > 0) {
-                require(result < type(uint256).max);
-                result++;
-            }
+        result = mulDiv(a, b, denominator);
+        if (mulmod(a, b, denominator) > 0) {
+            result++;
         }
     }
 }
