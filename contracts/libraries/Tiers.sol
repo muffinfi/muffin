eae1793245df49264d5c883f39df58d5a073434a
dkenw <dkenw1618@gmail.com>
2022-03-04 17:40:31 +0800

    fix cvf-163,164: document Tier struct

--- contracts/libraries/Tiers.sol
+++ contracts/libraries/Tiers.sol
@@ -5,10 +5,10 @@ library Tiers {
     struct Tier {
         uint128 liquidity;
         uint128 sqrtPrice; // UQ56.72
-        uint24 sqrtGamma;
+        uint24 sqrtGamma; // 5 decimal places
         int24 tick;
-        int24 nextTickBelow;
-        int24 nextTickAbove;
+        int24 nextTickBelow; // the next lower tick to cross (note that it can be equal to `tier.tick`)
+        int24 nextTickAbove; // the next upper tick to cross
         uint80 feeGrowthGlobal0; // UQ16.64
         uint80 feeGrowthGlobal1; // UQ16.64
     }
