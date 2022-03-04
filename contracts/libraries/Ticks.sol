888b77ead82250b0fb43b96a7ccd7bfe64b06957
dkenw <dkenw1618@gmail.com>
2022-03-04 15:47:02 +0800

    fix cvf-162: add more comments to explain "outside"

--- contracts/libraries/Ticks.sol
+++ contracts/libraries/Ticks.sol
@@ -9,8 +9,8 @@ library Ticks {
      * @param nextAbove         Next initialized tick above this tick
      * @param needSettle0       True if needed to settle positions with lower tick boundary at this tick (i.e. 1 -> 0 limit orders)
      * @param needSettle1       True if needed to settle positions with upper tick boundary at this tick (i.e. 0 -> 1 limit orders)
-     * @param feeGrowthOutside0 Fee0 growth per unit liquidity from this tick outwards (UQ16.64)
-     * @param feeGrowthOutside1 Fee1 growth per unit liquidity from this tick outwards (UQ16.64)
+     * @param feeGrowthOutside0 Fee0 growth per unit liquidity from this tick to the end in a direction away from the tier's current tick (UQ16.64)
+     * @param feeGrowthOutside1 Fee1 growth per unit liquidity from this tick to the end in a direction away from the tier's current tick (UQ16.64)
      * @param secondsPerLiquidityOutside Seconds per unit liquidity from this tick outwards (UQ8.80)
      */
     struct Tick {
