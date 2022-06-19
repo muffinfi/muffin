8d81899bc3313d0eab69cd2f7c07f6aa71d08cdd
dkenw <dkenw1618@gmail.com>
2022-06-20 00:59:28 +0800

    update demo script

--- contracts/tests/MockERC20.sol
+++ contracts/tests/MockERC20.sol
@@ -5,7 +5,17 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract MockERC20 is ERC20, ERC20Permit {
+    uint8 internal _decimals = 18;
+
     constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
+
+    function setDecimals(uint8 __decimals) public {
+        _decimals = __decimals;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 
     function mint(uint256 amount) public {
         _mint(msg.sender, amount);
--- scripts/demo.ts
+++ scripts/demo.ts
@@ -1,7 +1,7 @@
 import { BigNumber, constants } from 'ethers';
 import { defaultAbiCoder, keccak256 } from 'ethers/lib/utils';
 import { ethers, network } from 'hardhat';
-import { Manager, MockERC20, MuffinHub, MuffinHubPositions, WETH9 } from '../typechain';
+import { Manager, MockERC20, MuffinHub, IMuffinHubCombined, MuffinHubPositions, WETH9 } from '../typechain';
 import { bn, deploy, logTx, printStruct, wad } from './utils';
 
 /**
@@ -22,60 +22,67 @@ async function main() {
 
   // 2. deploy mock tokens
   const weth = (await ethers.getContractAt('WETH9', WETH_ADDRESS)) as WETH9;
-  const wbtc = (await deploy('MockERC20', 'WBTC', 'WBTC')) as MockERC20;
-  const usdc = (await deploy('MockERC20', 'USDC', 'USDC')) as MockERC20;
+  const wbtc = (await deploy('MockERC20', 'Wrapped BTC', 'WBTC')) as MockERC20;
+  const usdc = (await deploy('MockERC20', 'USD Coin', 'USDC')) as MockERC20;
+  await logTx(wbtc.setDecimals(8), 'wbtc.setDecimals');
+  await logTx(usdc.setDecimals(6), 'usdc.setDecimals');
 
   // 3. deploy contracts
-  const hub = (await deploy('MuffinHub', (await deploy('MuffinHubPositions')).address)) as MuffinHub;
+  const _hub = (await deploy('MuffinHub', (await deploy('MuffinHubPositions')).address)) as MuffinHub;
+  const hub = (await ethers.getContractAt('IMuffinHubCombined', _hub.address)) as IMuffinHubCombined;
   const manager = (await deploy('Manager', hub.address, weth.address)) as Manager;
 
   // 4. mint and approve tokens
-  await logTx(usdc.mint(wad(10000)), 'mint usdc');
-  await logTx(wbtc.mint(wad(10000)), 'mint wbtc');
-  await logTx(usdc.approve(manager.address, ethers.constants.MaxUint256), 'approve usdc to manager');
+  await logTx(wbtc.mint(10000e8), 'mint wbtc');
+  await logTx(usdc.mint(10000e6), 'mint usdc');
   await logTx(wbtc.approve(manager.address, ethers.constants.MaxUint256), 'approve wbtc to manager');
+  await logTx(usdc.approve(manager.address, ethers.constants.MaxUint256), 'approve usdc to manager');
 
   // 5. deposit tokens for creating pool
-  await logTx(manager.deposit(user.address, wbtc.address, wad(1)), 'deposit wbtc');
-  await logTx(manager.deposit(user.address, usdc.address, wad(1)), 'deposit usdc');
+  await logTx(manager.deposit(user.address, wbtc.address, 1e8), 'deposit wbtc');
+  await logTx(manager.deposit(user.address, usdc.address, 1e6), 'deposit usdc');
 
   // 6. create pool
-  const btcUsdPrice = bn(1).shl(144); // assume btc:usd = 1:1
+  const btcUsdPrice = bn(1e6).shl(144).div(1e8); // assume btc:usd = 1:1
   const [token0, token1, price] =
     wbtc.address.toLowerCase() < usdc.address.toLowerCase()
       ? [wbtc, usdc, btcUsdPrice]
       : [usdc, wbtc, bn(1).shl(288).div(btcUsdPrice)];
-  await logTx(manager.createPool(token0.address, token1.address, 99850, sqrt(price)), 'create pool + add tier 30 bps');
+  const poolId = keccak256(defaultAbiCoder.encode(['address', 'address'], [token0.address, token1.address]));
+  await logTx(hub.setPoolAllowedSqrtGammas(poolId, [99975, 99940, 99875, 99800]), 'set pool sqrt gammas whitelist');
+  await logTx(
+    manager.createPool(token0.address, token1.address, 99975, sqrt(price), false, { gasLimit: 600_000 }),
+    'create pool + add tier 5 bps',
+  );
 
   // 7. add some more tiers
-  await logTx(manager.depositToExternal(user.address, 1, token0.address, wad(1)), 'deposit token0 externally');
-  await logTx(manager.depositToExternal(user.address, 1, token1.address, wad(1)), 'deposit token1 externally');
-  await logTx(hub.addTier(token0.address, token1.address, 99925, 1, { gasLimit: 500000 }), 'add tier 15 bps');
-  await logTx(hub.addTier(token0.address, token1.address, 99975, 1, { gasLimit: 500000 }), 'add tier  5 bps');
+  await logTx(manager.addTier(token0.address, token1.address, 99940, false, { gasLimit: 500000 }), 'add tier 12 bps');
+  await logTx(manager.addTier(token0.address, token1.address, 99875, false, { gasLimit: 500000 }), 'add tier 25 bps');
+  await logTx(manager.addTier(token0.address, token1.address, 99800, false, { gasLimit: 500000 }), 'add tier 40 bps');
 
   // 8. set pool's tick spacing to 1, protocol fee to 15%
-  const poolId = keccak256(defaultAbiCoder.encode(['address', 'address'], [token0.address, token1.address]));
-  const _hub = (await ethers.getContractAt('MuffinHubPositions', hub.address)) as MuffinHubPositions;
-  await _hub.setPoolParameters(poolId, 1, Math.floor(0.15 * 255));
+  await logTx(hub.setPoolParameters(poolId, 25, Math.floor(0.15 * 255)), 'set pool parameters');
 
-  // 9. set tier's limit order tick spacing to 2 ticks
-  await _hub.setTierParameters(poolId, 0, 99850, 200);
+  // 9. set tier's limit order tick spacing to 1 tickspacing
+  await logTx(hub.setTierParameters(poolId, 0, 99975, 1), 'set tier parameters');
 
   // 10. add liquidity
-  let promise = manager.mint({
-    token0: token0.address,
-    token1: token1.address,
-    tierId: 0,
-    tickLower: -100,
-    tickUpper: 100,
-    amount0Desired: wad(1),
-    amount1Desired: wad(1),
-    amount0Min: 0,
-    amount1Min: 0,
-    recipient: user.address,
-    useAccount: false,
-  });
-  await logTx(promise, 'add liquidity');
+  await logTx(
+    manager.mint({
+      token0: token0.address,
+      token1: token1.address,
+      tierId: 0,
+      tickLower: 0,
+      tickUpper: 25,
+      amount0Desired: 1e8,
+      amount1Desired: 1e8,
+      amount0Min: 0,
+      amount1Min: 0,
+      recipient: user.address,
+      useAccount: false,
+    }),
+    'add liquidity',
+  );
 
   // 11. fetch position info
   const tokenId = 1;
@@ -86,43 +93,44 @@ async function main() {
 
   // 12. swap (exact in)
   await logTx(
-    manager.exactInSingle(token0.address, token1.address, 0b111111, 1e8, 0, user.address, false, false, constants.MaxUint256),
+    manager.exactInSingle(token0.address, token1.address, 0b111111, 100, 0, user.address, false, false, constants.MaxUint256),
     'swap',
   );
 
   // 13. remove liquidity
-  promise = manager.removeLiquidity({
-    tokenId,
-    liquidityD8: 1000,
-    amount0Min: 0,
-    amount1Min: 0,
-    withdrawTo: user.address,
-    collectAllFees: true,
-    settled: false,
-  });
-  await logTx(promise, 'remove liquidity');
+  await logTx(
+    manager.removeLiquidity({
+      tokenId,
+      liquidityD8: 1000,
+      amount0Min: 0,
+      amount1Min: 0,
+      withdrawTo: user.address,
+      collectAllFees: true,
+      settled: false,
+    }),
+    'remove liquidity',
+  );
 
   // 14. set position to limit order
   await logTx(manager.setLimitOrderType(tokenId, 1), 'set limit order'); // 1 means zero for one
 
   // 15. create a weth-usdc pool (for subgraph)
   {
-    const ethUsdPrice = bn(2500).shl(144);
+    const ethUsdPrice = bn(2500e6).shl(144).div(bn(10).pow(18));
     const [token0, token1, price] =
       weth.address.toLowerCase() < usdc.address.toLowerCase()
         ? [weth, usdc, ethUsdPrice]
         : [usdc, weth, bn(1).shl(288).div(ethUsdPrice)];
+    const poolId = keccak256(defaultAbiCoder.encode(['address', 'address'], [token0.address, token1.address]));
 
-    await logTx(weth.deposit({ value: 1e8 }), 'mint weth');
-    await logTx(weth.approve(hub.address, constants.MaxUint256), 'approve weth to manager');
+    await logTx(weth.deposit({ value: 1e12 }), 'mint weth');
+    await logTx(weth.approve(manager.address, constants.MaxUint256), 'approve weth to manager');
+    await logTx(hub.setPoolAllowedSqrtGammas(poolId, [99975, 99940, 99875, 99800]), 'set pool sqrt gammas whitelist');
     await logTx(
-      manager.createPool(token0.address, token1.address, 99850, sqrt(price), { gasLimit: 600_000 }),
+      manager.createPool(token0.address, token1.address, 99975, sqrt(price), false, { gasLimit: 600_000 }),
       'create weth-usdc pool',
     );
   }
-
-  // 16. perform a swap
-  await manager.exactInSingle(usdc.address, wbtc.address, 0x3f, 100, 0, user.address, false, true, constants.MaxUint256);
 }
 
 main()
